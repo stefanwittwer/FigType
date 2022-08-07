6497fee1d12c34fcdbc7fd518f5d44d30419efd5
Stefan Wittwer <stefan.wittwer@nextbusiness.ch>
2022-08-07 23:21:21 +0200

    Improved empty state explanatory UI instructions

--- src/components/PropertiesEmptyState.tsx
+++ src/components/PropertiesEmptyState.tsx
@@ -6,7 +6,7 @@ const PropertiesEmptyState = () => (
       <PlaceholderRow opacity={0.3} />
     </AutoLayout>
     <Text fontSize={12} opacity={0.6} width={370} lineHeight={20}>
-      Pick a type and click the plus icon in the widget menu to add your first property.
+      Pick a type from the widget menu and click the plus icon to add your first property.
     </Text>
   </AutoLayout>
 )
