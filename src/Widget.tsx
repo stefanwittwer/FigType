9de5626cb22ab6461793e7ce6d47d7bbc35dcebe
Fabian Gassen <gassen@salient-doremus.de>
2022-09-08 14:26:22 +0200

    Add relation functionality to Widget

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -9,22 +9,41 @@ const {
   SVG,
   Text,
   waitForTask,
+  Line,
 } = widget
 
 const Widget = () => {
   const [title, setTitle] = useSyncedState("title", "")
+  const [entityId, setEntityId] = useSyncedState("entityId", "")
   const [description, setDescription] = useSyncedState("description", "")
   const [colour, setColour] = useSyncedState<string>("colour", EntityColours.None)
 
   const [propertyIds, setPropertyIds] = useSyncedState<string[]>("propertyKeys", [])
   const properties = useSyncedMap<Property>("properties")
 
+  const [relationIds, setRelationIds] = useSyncedState<string[]>("relationKeys", [])
+  const relations = useSyncedMap<Relation>("relations")
+  const [availableEntities, setAvailableEntities] = useSyncedState("availableEntities", [])
+
   const [typeToAdd, setTypeToAdd] = useSyncedState<PropertyType>("typeToAdd", "string")
+
+  const assignEntityId = () => {
+    setEntityId(randomId())
+  }
 
   const addProperty = () => {
     const propertyId = randomId()
-    properties.set(propertyId, { title: "", type: typeToAdd })
-    setPropertyIds([...propertyIds, propertyId])
+    if (typeToAdd === "relation") {
+      let collectedEntities = []
+      figma.root.children.forEach(page => {
+        collectedEntities = [...collectedEntities, ...page.findWidgetNodesByWidgetId(figma.widgetId).map(widget => widget.widgetSyncedState)]
+      })
+
+      setAvailableEntities(collectedEntities)
+    } else {
+      properties.set(propertyId, {title: "", type: typeToAdd})
+      setPropertyIds([...propertyIds, propertyId])
+    }
   }
   const deleteProperty = (propertyToDelete: string) => {
     properties.delete(propertyToDelete)
@@ -38,6 +57,58 @@ const Widget = () => {
     const cpy = [...propertyIds]
     ;[cpy[idx], cpy[idx + offset]] = [cpy[idx + offset], cpy[idx]]
     setPropertyIds(cpy)
+  }
+
+  const addRelation = (entityToAdd: any) => {
+    const relationId = randomId()
+    const properties:any = []
+    const relatedPropertyIds = entityToAdd["propertyKeys"]
+    const relatedProperties = entityToAdd["properties"]
+
+    relatedPropertyIds.forEach(propertyId => {
+      properties.push({...relatedProperties[propertyId], propertyId: propertyId})
+    })
+
+    const relation: Relation = {
+      title: "",
+      relatedEntity: entityToAdd["entityId"],
+      relatedTitle: entityToAdd["title"],
+      relatedProperties: properties,
+      relatedPropertiesSelected: relatedPropertyIds,
+      type: typeToAdd
+    }
+
+    relations.set(relationId, relation)
+    setRelationIds([...relationIds, relationId])
+    setAvailableEntities([])
+  }
+
+  const toggleRelatedProperty = (relationId: string, propertyId: string) => {
+    const relation = relations.get(relationId)
+    const index = relation.relatedPropertiesSelected.findIndex(id => id === propertyId)
+
+    if (index === -1) {
+      relation.relatedPropertiesSelected = [...relation.relatedPropertiesSelected, propertyId]
+    } else {
+      relation.relatedPropertiesSelected.splice(index, 1)
+    }
+
+    relations.set(relationId, relation)
+  }
+
+  const deleteRelation = (relationToDelete: string) => {
+    relations.delete(relationToDelete)
+    setRelationIds([...relationIds].filter((relationId) => relationId !== relationToDelete))
+  }
+
+  const moveRelation = (relationId: string, offset: 1 | -1) => {
+    const idx = relationIds.indexOf(relationId)
+    if (idx == -1 || idx + offset >= relationIds.length || idx + offset <= -1) {
+      return
+    }
+    const cpy = [...relationIds]
+    ;[cpy[idx], cpy[idx + offset]] = [cpy[idx + offset], cpy[idx]]
+    setRelationIds(cpy)
   }
 
   usePropertyMenu(
@@ -88,7 +159,10 @@ const Widget = () => {
       <EnitityDetails
         title={title}
         description={description}
-        setTitle={setTitle}
+        setTitle={(title) => {
+          setTitle(title)
+          assignEntityId()
+        }}
         setDescription={setDescription}
       />
       {propertyIds.length === 0 ? (
@@ -102,6 +176,30 @@ const Widget = () => {
           moveUpProperty={(propertyId) => moveProperty(propertyId, -1)}
         />
       )}
+
+      <Line strokeWidth={2} length={370} opacity={0.1} />
+
+      {availableEntities.length !== 0 &&
+          <SelectEntity onSelect={addRelation} availableEntities={availableEntities} />
+      }
+
+      {
+          availableEntities.length === 0 &&
+          relationIds.length === 0 &&
+          propertyIds.length === 0 &&
+          <RelationsEmptyState />
+      }
+
+      {relationIds.length !== 0 &&
+          <RelationList
+              relations={relations}
+              relationIds={relationIds}
+              deleteRelation={deleteRelation}
+              moveDownRelation={(relationId) => moveRelation(relationId, 1)}
+              moveUpRelation={(relationId) => moveRelation(relationId, -1)}
+              toggleRelatedProperty={toggleRelatedProperty}
+          />
+      }
     </WidgetContainer>
   )
 }
